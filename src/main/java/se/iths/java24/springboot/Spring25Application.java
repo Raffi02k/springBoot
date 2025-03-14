1bd1ad07b351de219d75c0fddc7fc18c17624f50
Raffi <raffi9998@gmail.com>
2025-03-14 14:59:48 +0100

    Spring boot git push

--- src/main/java/se/iths/java24/springboot/Spring25Application.java
+++ src/main/java/se/iths/java24/springboot/Spring25Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Spring25Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Spring25Application.class, args);
     }
 
 }
